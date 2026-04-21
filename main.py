284457f3da7072ab7899c5e6b2176a39bc360f90
valentinomusaschi <117128638+valentinomusaschi@users.noreply.github.com>
2026-04-21 20:02:28 -0300

    Create main.py
    Agrego archivo principal main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from valentino import sumar, restar
+from lucio import multiplicar, dividir
+
+print("Suma:", sumar(5, 3))
+print("Resta:", restar(5, 3))
+print("Multiplicación:", multiplicar(5, 3))
+print("División:", dividir(5, 3))
